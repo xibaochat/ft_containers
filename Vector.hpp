693ab7a00adf0a635b0eaa12cbeec435cece3a75
xibaochat <wmmxxbb@gmail.com>
2021-08-27 01:43:07 +0200

    add part of vector

--- Vector.hpp
+++ Vector.hpp
@@ -66,9 +66,9 @@ namespace ft
 		//post-decrease :a--
 		_Self operator--(int)
 		{
-				_Self obj(*this);
-				m_ptr--;
-				return (obj);
+			_Self obj(*this);
+			m_ptr--;
+			return (obj);
 		}
 
 		_Self & operator+=(size_type n)
@@ -78,13 +78,11 @@ namespace ft
 		}
 
 		_Self &operator-=(size_type n)
-		{
+			{
 			m_ptr -= n;
 			return (*this);
 		}
 		reference operator[](size_type n){return m_ptr[n];}
-
-
 	};
 
 	template <typename T>
@@ -113,49 +111,90 @@ namespace ft
 	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-template< class T, class Allocator = std::allocator<T> >
+	template< class T, class Allocator = std::allocator<T> >
 	class Vector
 	{
 	public:
-		typedef T             value_type;
-		typedef Allocator     allocator_type;
-		typedef size_t size_type;
-
-	private:
+		typedef T                       value_type;
+		typedef Allocator               allocator_type;
+		typedef size_t                  size_type;
+		typedef std::ptrdiff_t          difference_type;
+		typedef T&                      reference;
+		typedef const T&                const_reference;
+		typedef T*                      pointer;
+		typedef const T*                const_pointer;
+		typedef VectorIterator<T>       iterator;
+		typedef VectorIterator<const T> const_iterator;
 		T *_arr;
+	private:
+
 		size_type _len;
 		size_type _cap;
 	public:
 		explicit Vector (const allocator_type& alloc = allocator_type()):_arr(NULL), _len(0), _cap(0){}
 
 		explicit Vector (size_type n, const value_type& val = value_type(),
-						 const allocator_type& alloc = allocator_type()):_arr(NULL), _len(0), _cap(0){}
+						 const allocator_type& alloc = allocator_type()):_arr(NULL), _len(0), _cap(0)
+			{
+//				insert(begin(), n, val);
+			}
 
 		template <class InputIterator>
 		Vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){}
+
+		iterator insert (iterator position, const value_type& val)
+		{
+			insert(position, 1, val);
+			return (position);
+		}
+		void insert (iterator position, size_type n, const value_type& val)
+		{
+			std::cout <<  "ICI\n";
+			if (!n)
+				return ;
+			std::ptrdiff_t index = position - _arr;
+			std::cout << index << "\n";
+			size_type tmp = index;
+			if (n + _len > _cap)
+				reserve(_len + n);
+			while (index < _len)
+			{
+				_arr[index + n] = _arr[index];
+				index++;
+			}
+			index = tmp;
+			while (n > 0)
+			{
+					_arr[index] = val;
+					n--;
+			}
+			_len += n;
+			for(int i=0; i < _len; i++)
+				std::cout << " " << _arr[i] << "\n";
+		}
+
+		template <class InputIterator>
+		void insert (iterator position, InputIterator first, InputIterator last)
+		{
+			std::cout << "je suis la\n";
+
+		}
+		void reserve (size_type n)
+		{
+			if (n > _cap)
+			{
+				allocator_type alloc;
+				pointer new_arr = alloc.allocate(n);
+				for(size_type i=0; i < _len; i++)
+				{
+					alloc.construct(new_arr + i, _arr[i]);
+					alloc.destroy(_arr + i);
+				}
+				alloc.deallocate(_arr, _cap);
+				_arr = new_arr;
+				_cap = n;
+			}
+		}
 
 
 
